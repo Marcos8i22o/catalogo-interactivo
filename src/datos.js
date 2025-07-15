b15a12b28df84e1f4f76311cc257105f366be9c8
Marcos <marcos.jinno@gmail.com>
2025-07-15 12:41:34 -0300

    Crea el componente TarjetaProducto

--- src/datos.js
+++ src/datos.js
@@ -1,37 +1,100 @@
 export const productos = [
   {
     "id": 1,
-    "nombre": "Camiseta Deportiva",
-    "categoria": "Ropa",
-    "precio": 19.99,
-    "imagen": "https://via.placeholder.com/150?text=Camiseta"
+    "nombre": "Monitor",
+    "categoria": "Electrónica",
+    "precio": 199.99,
+    "imagen": "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
   },
   {
     "id": 2,
-    "nombre": "Auriculares Bluetooth",
+    "nombre": "SSD Portátil",
     "categoria": "Electrónica",
-    "precio": 49.99,
-    "imagen": "https://via.placeholder.com/150?text=Auriculares"
+    "precio": 89.99,
+    "imagen": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg"
   },
   {
     "id": 3,
-    "nombre": "Silla Ergonómica",
-    "categoria": "Muebles",
-    "precio": 129.99,
-    "imagen": "https://via.placeholder.com/150?text=Silla"
+    "nombre": "Disco Duro Externo",
+    "categoria": "Electrónica",
+    "precio": 64.99,
+    "imagen": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg"
   },
   {
     "id": 4,
-    "nombre": "Cafetera Espresso",
-    "categoria": "Electrodomésticos",
-    "precio": 89.99,
-    "imagen": "https://via.placeholder.com/150?text=Cafetera"
+    "nombre": "Laptop",
+    "categoria": "Electrónica",
+    "precio": 499.99,
+    "imagen": "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg"
   },
   {
     "id": 5,
-    "nombre": "Zapatos Running",
-    "categoria": "Calzado",
+    "nombre": "Anillo",
+    "categoria": "Joyería",
+    "precio": 129.99,
+    "imagen": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 6,
+    "nombre": "Brazalete",
+    "categoria": "Joyería",
+    "precio": 79.99,
+    "imagen": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  },
+  {
+    "id": 7,
+    "nombre": "Pulsera de oro blanco",
+    "categoria": "Joyería",
+    "precio": 149.99,
+    "imagen": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
+  },
+  {
+    "id": 8,
+    "nombre": "Chaqueta de plumas",
+    "categoria": "Ropa de Hombre",
     "precio": 59.99,
-    "imagen": "https://via.placeholder.com/150?text=Zapatos"
+    "imagen": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
+  },
+  {
+    "id": 9,
+    "nombre": "Camiseta casual",
+    "categoria": "Ropa de Hombre",
+    "precio": 24.99,
+    "imagen": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  },
+  {
+    "id": 10,
+    "nombre": "Camiseta de algodón",
+    "categoria": "Ropa de Hombre",
+    "precio": 19.99,
+    "imagen": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
+  },
+  {
+    "id": 11,
+    "nombre": "Chaqueta de motociclista",
+    "categoria": "Ropa de Mujer",
+    "precio": 69.99,
+    "imagen": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
+  },
+  {
+    "id": 12,
+    "nombre": "Chubasquero con capucha",
+    "categoria": "Ropa de Mujer",
+    "precio": 49.99,
+    "imagen": "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"
+  },
+  {
+    "id": 13,
+    "nombre": "Vestido de manga corta",
+    "categoria": "Ropa de Mujer",
+    "precio": 39.99,
+    "imagen": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
+  },
+  {
+    "id": 14,
+    "nombre": "Blusa de manga corta",
+    "categoria": "Ropa de Mujer",
+    "precio": 29.99,
+    "imagen": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
   }
 ]
