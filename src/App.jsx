2b834b95a80290df7d64477c0ba35e443a8a9387
Marcos <marcos.jinno@gmail.com>
2025-07-17 12:32:14 -0300

    Añade componente Encabezado con un campo de búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import Encabezado from './componentes/Encabezado';
 import TarjetaProducto from './componentes/TarjetaProducto';
 import { productos } from './datos';
 
 function App() {
   return (
-    <div className='contenedor-productos'>
-      {productos.map((producto) => (
-        <TarjetaProducto
-          nombre={producto.nombre}
-          precio={producto.precio}
-          imagen={producto.imagen}
-        />
-      ))}
-    </div>
+    <>
+      <Encabezado />
+      <div className="contenedor-productos">
+        {productos.map((producto) => (
+          <TarjetaProducto
+            nombre={producto.nombre}
+            precio={producto.precio}
+            imagen={producto.imagen}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/componentes/Encabezado.jsx
@@ -0,0 +1,10 @@
+function Encabezado() {
+  return (
+    <div>
+      <h2>Buscar producto</h2>
+      <input type="text"/>
+    </div>
+  );
+}
+
+export default Encabezado;
