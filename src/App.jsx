da31be9460f42a06d5902d26dd07e74b416b45af
Marcos <marcos.jinno@gmail.com>
2025-07-22 11:27:38 -0300

    Muestra un mensaje cuando no hay resultados de búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,15 @@ function App() {
     ...new Set(productos.map((producto) => producto.categoria)),
   ];
 
-  return (
-    <>
-      <Encabezado
-        categorias={categoriasUnicas}
-        onChange={(inputTexto) => {
-          setInputTexto(inputTexto);
-        }}
-        onCategoriaClick={setCategoriaSeleccionada}
-      />
-      <div className="contenedor-productos">
-        {productos
-          .filter((producto) =>
-            (producto.nombre.toLowerCase().includes(inputTexto.toLowerCase()) && (categoriaSeleccionada === "Todos" || producto.categoria === categoriaSeleccionada)),
+  const productosFiltrados = productos
+          .filter(
+            (producto) =>
+              producto.nombre
+                .toLowerCase()
+                .includes(inputTexto.toLowerCase()) &&
+              (categoriaSeleccionada === 'Todos' ||
+                producto.categoria === categoriaSeleccionada),
+                
           )
           .map((producto) => (
             <TarjetaProducto
@@ -33,8 +29,24 @@ function App() {
               imagen={producto.imagen}
               descripcion={producto.descripcion}
             />
-          ))}
+            
+          ))
+
+  return (
+    <>
+    
+      <Encabezado
+        categorias={categoriasUnicas}
+        onChange={(inputTexto) => {
+          setInputTexto(inputTexto);
+        }}
+        onCategoriaClick={setCategoriaSeleccionada}
+      />
+      <div className="contenedor-productos">
+        {productosFiltrados.length !== 0 ? productosFiltrados : <h4>No se encontraron resultados</h4>}
+        
       </div>
+
     </>
   );
 }
